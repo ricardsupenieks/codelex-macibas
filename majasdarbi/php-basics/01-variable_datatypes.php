1f273733d0a14fb2ab974d3a3c718b63a9b749d5
ricardsupenieks <110990211+ricardsupenieks@users.noreply.github.com>
2022-10-27 16:17:47 +0300

    Add files via upload

---
+++ majasdarbi/php-basics/01-variable_datatypes.php
@@ -0,0 +1,16 @@
+<?php
+$integer = 10;
+$float = 10.10;
+$string = "hello world";
+
+//var_dump($integer);
+//var_dump($float);
+//var_dump($string);
+
+$name = "bob";
+$surname = "bobson";
+$age = 7;
+
+$concatination = $name . " " . $surname . " " . "age:" . $age;
+echo $concatination;
+
