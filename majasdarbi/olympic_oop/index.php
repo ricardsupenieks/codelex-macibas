b63fb1fb729666a5d5ce3ec675638fb257dd4807
ricardsupenieks <110990211+ricardsupenieks@users.noreply.github.com>
2022-11-22 21:49:50 +0200

    Update index.php

--- majasdarbi/olympic_oop/index.php
+++ majasdarbi/olympic_oop/index.php
@@ -1,41 +1,92 @@
 <?php
 
-require_once "app/Screen.php";
-require_once "app/Symbols.php";
-require_once "app/Game.php";
+require_once "vendor/autoload.php";
+
+use App\Symbols;
+use App\Combinations;
+
+$symbols = new Symbols();
+$combinations = new Combinations();
+
+$screen = [
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    ];
+
+function displayScreen() : string{
+    global $screen;
+    return "_____________________ \n" .
+        "| {$screen[0][0]} | {$screen[0][1]} | {$screen[0][2]} | {$screen[0][3]} | {$screen[0][4]} | \n" .
+        "--------------------- \n" .
+        "| {$screen[1][0]} | {$screen[1][1]} | {$screen[1][2]} | {$screen[1][3]} | {$screen[1][4]} | \n" .
+        "--------------------- \n" .
+        "| {$screen[2][0]} | {$screen[2][1]} | {$screen[2][2]} | {$screen[2][3]} | {$screen[2][4]} | \n" .
+        "--------------------- \n" . PHP_EOL;
+}
+
+
+function winner(){
+    global $combinations, $symbols, $screen, $totalCashWon;
+
+    foreach ($combinations->getCombinations() as $combination) {
+        $combinationCounter = 0;
+
+        [$symbolX, $symbolY] = $combination[0];
+        $symbol = $screen[$symbolX][$symbolY];
+
+        foreach ($combination as $position) {
+            [$x, $y] = $position;
+            if ($screen[$x][$y] !== $symbol) {
+                break;
+            }
+            $combinationCounter++;
+        }
+        if ($combinationCounter === 15 || $combinationCounter === 10 || $combinationCounter === 5) {
+            echo "WIN WIN WIN" . PHP_EOL;
+            $win = $symbols->getSymbols()[$symbol] * $combinationCounter / 5;
+            $totalCashWon[] = $win;
+            echo "You've won $win$!" . PHP_EOL;
+        }
+    }
+}
+
+function insertSymbol(int $rows, int $columns): void
+{
+    global $screen, $symbols;
+    for ($i = 0; $i < $rows; $i++) {
+        for ($j = 0; $j < $columns; $j++) {
+            $screen[$i][$j] = $symbols->getRandomSymbol();
+        }
+    }
+}
 
 echo "Each spin will cost 100$" . PHP_EOL;
 
-$screen = new Screen();
-$symbols = new Symbols();
-$game = new Game();
-
 while (true) {
-    $game->setPlayerCash(readline("Enter amount of cash you will play with (min = 100): "));
-    while ($game->getPlayerCash() < 100) {
-        $game->setPlayerCash(readline("Please enter more or equal than the minimum amount (100): "));
+    $playerCash = readline("Enter the amount of money you will play with (min = 100): ");
+    while ($playerCash < 100) {
+        $playerCash = readline("Please enter more or equal than the minimum amount (100): ");
     }
 
     $win = 0;
 
-    $timesSpun = $game->getTimesSpun();
+    $timesSpun = 1;
 
-    $totalCashWon = $game->getTotalCashWon();
+    $totalCashWon = [];
 
     do {
-        for ($i = 0; $i < count($screen->getScreen()); $i++) {
-            $screen->getWindow($i);
-        }
 
-        echo $screen->displayScreen();
+        insertSymbol(3,5);
 
-        $game->win();
+        echo displayScreen();
+
+        winner();
 
         echo "Player balance: ";
-        $cash = $game->setPlayerCash($game->getPlayerCash() - 100 * $game->getTimesSpun() + array_sum($game->getTotalCashWon()));
-        echo $cash . PHP_EOL;
+        echo $playerCash - 100 * $timesSpun + array_sum($totalCashWon) . PHP_EOL;
 
-        if ($cash < 100) {
+        if ($playerCash - 100 * $timesSpun + array_sum($totalCashWon) < 100) {
             echo "Not enough cash to continue playing" . PHP_EOL;
             echo "Would you like to insert more cash?  Yes (Enter) \ No (Any button)";
             echo PHP_EOL;
@@ -55,7 +106,7 @@ while (true) {
         if ($input !== "") {
             exit;
         }
-        $game->setTimesSpun(+1);
+        $timesSpun++;
 
-    } while ($game->getPlayerCash() >= 100);
+    } while ($playerCash > 99);
 }
