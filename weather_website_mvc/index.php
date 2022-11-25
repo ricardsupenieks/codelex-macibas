91a9498bde68961c0c912624ac4d76de13604fb0
ricardsupenieks <110990211+ricardsupenieks@users.noreply.github.com>
2022-11-25 10:56:40 +0200

    Create index.php

---
+++ weather_website_mvc/index.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+require_once "vendor/autoload.php";
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $route) {
+    $route->addRoute('GET', '/', [\App\Controllers\PersonsController::class, 'index']);
+    $route->addRoute('GET', '/persons', [\App\Controllers\PersonsController::class, 'index']);
+    $route->addRoute('GET', '/articles', [\App\Controllers\ArticlesController::class, 'index']);
+    $route->addRoute('GET', '/articles/create', [\App\Controllers\ArticlesController::class, 'create']);
+
+
+    // METHOD + URL
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+
+        [$controller, $method] = $handler;
+
+//        $controller = 'App\Controllers\\' . ucfirst($controller) . 'Controller';
+
+
+        $response = (new $controller)->{$method}();
+        break;
+}
+
+?>
